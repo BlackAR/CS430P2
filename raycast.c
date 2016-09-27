96a4f04289436ed4341f5aa7b3a272049de5ba35
Anthony Black <anthony.r.black13@gmail.com>
2016-09-27 12:51:14 -0700

    Initial c file

---
+++ raycast.c
@@ -0,0 +1,16 @@
+/*
+ ============================================================================
+ Name        : raycast.c
+ Author      : Anthony Black
+ Description : CS430 Project 2: Raycaster
+ ============================================================================
+ */
+
+#include <stdio.h>
+#include <stdlib.h>
+
+int main(int argc, char *argv[]) {
+	puts("%d", argc);
+	puts("!!!Hello World!!!"); /* prints !!!Hello World!!! */
+	return EXIT_SUCCESS;
+}
